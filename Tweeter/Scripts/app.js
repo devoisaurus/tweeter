a61b542d940aa6910df6ccff3cb20124d73a5954
Andi DeVois <adevois@gmail.com>
2016-11-16 16:00:49 -0800

    preventing merge conflicts

---
+++ Tweeter/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿$("#register - username").focusout(function () {
+    $.ajax({
+        url: "/api/TwitUsername/" + $(this).val(),
+        method: 'GET'
+    }).success(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+});
